2d98ffb0589cf0bea8c3f3b08f3328bd2c0ba541
mac <mac@macs-iMac-2.local>
2018-05-08 21:57:23 +0500

    all file attach with single css

---
+++ mnbFinal/js/learning/chap1.js
@@ -0,0 +1,18 @@
+// function foo() {
+//     return "Hello world!";
+// }
+// var c = "";
+// var b = 2;
+// var [a] = [ [ [b] * [2] ] + [ [foo] [ [c] *  [3] ] ] ];
+// var a = b *2 + foo(c * 2);
+//
+// print(a);
+
+// JS Basics
+// Declaring variables
+// Creating and invoking functions
+
+// ES2015
+// Class syntax
+// Arrow functions
+// Spread operator
